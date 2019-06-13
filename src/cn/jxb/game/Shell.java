1bb7295ae217caa5bdd280e2912ae82fcca73dbe
mrjiangxb <mrjiangxb@163.com>
2019-06-13 18:22:06 +0800

    Java基础实战之飞机小游戏

---
+++ src/cn/jxb/game/Shell.java
@@ -0,0 +1,39 @@
+package cn.jxb.game;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+/**
+ * 炮弹类
+ * @author JXB
+ */
+public class Shell extends GameObject{
+	double degree;
+	
+	public Shell(){
+		x = 200;
+		y = 200;
+		width = 10;
+		height = 10;
+		speed = 3;
+		degree = Math.random()*Math.PI*2; //随机生成弧度
+	}
+	
+	public void draw(Graphics g){
+		Color c = g.getColor();
+		g.setColor(Color.YELLOW);
+		
+		g.fillOval((int)x, (int)y, width, height);
+		
+		//炮弹沿着任意角度去飞
+		x += speed*Math.cos(degree);
+		y += speed*Math.sin(degree);
+		
+		if(x<0 || x>Constant.GAME_WIDTH-width){
+			degree = Math.PI - degree;//沿纵坐标反弹
+		}
+		if(y<30 || y>Constant.GAME_HEIGHT-height){
+			degree = - degree; //沿横反弹
+		}
+	}
+}
